f6c2d58bd25a0eac403ea7cd34041c176a06f744
Utsavojha2 <utsavojha2@gmail.com>
2024-04-25 23:30:51 +0545

    Add links

---
+++ src/app/verify/page.tsx
@@ -0,0 +1,8 @@
+import VerifyAccount from "@src/components/VerifyAccount";
+import React from "react";
+
+const Verify = () => {
+  return <VerifyAccount />;
+};
+
+export default Verify;
---
+++ src/components/VerifyAccount.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const VerifyAccount = () => {
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+
+    const siteURL = queryParams.get("siteURL");
+    const accountToken = queryParams.get("token");
+    const type = queryParams.get("type");
+    const redirectTo = queryParams.get("redirect_to");
+
+    if (!siteURL || !accountToken || !type || !redirectTo) {
+      setIsError(true);
+    } else {
+      const accountVerificationURL = `${siteURL}?token=${accountToken}&type=${type}&redirect_to=${`${redirectTo}?token=${accountToken}`}`;
+      window.open(accountVerificationURL, "_self");
+    }
+  }, []);
+
+  return (
+    <div
+      style={{
+        marginTop: 50,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {isError ? (
+        <h2>Error redirecting to Journa application</h2>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
+};
+
+export default VerifyAccount;
